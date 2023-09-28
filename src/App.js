d76e571b5d2eb88f55f58e313c93dd8477395deb
Hemang <hemang2050@gmail.com>
2023-09-28 17:22:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -202,7 +202,7 @@ function App() {
   return (
     <div className="container mx-auto p-5">
       <h1 className="text-center text-3xl font-bold mb-14 ">
-        QCS STATISTICS DASHBOARD
+        STATISTICAL ANALYSIS DASHBOARD
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
         <div>
